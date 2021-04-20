6c0ff6de28e9e12041035092747797aaab107759
Mason Mackaman <masondeanm@aol.com>
2021-04-20 16:13:49 -0400

    Refactor flake

--- flake.nix
+++ flake.nix
@@ -1,18 +1,23 @@
-{ inputs.purs-nix.url = "github:ursi/purs-nix";
+{ inputs =
+    { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+      purs-nix.url = "github:ursi/purs-nix";
+      utils.url = "github:ursi/flake-utils";
+    };
 
-  outputs = { nixpkgs, utils, purs-nix, ... }:
+  outputs = { nixpkgs, purs-nix, utils, ... }:
     utils.defaultSystems
       ({ make-shell, pkgs, system }:
-         with pkgs;
          let
            pn = purs-nix { inherit system; };
            inherit (pn) purs ps-pkgs;
+           package = import ./package.nix pn;
 
            inherit
              (purs
-                { inherit (import ./package.nix pn) dependencies;
+                { inherit (package) dependencies;
 
                   test-dependencies =
+                    with ps-pkgs;
                     [ ps-pkgs.ansi
                       ps-pkgs."assert"
                     ];
